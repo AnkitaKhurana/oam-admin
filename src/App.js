64cc488815f6d6af9fb296846d4a95bc020c6384
sharkinsspatial <sharkinsgis@gmail.com>
2018-07-09 18:21:50 -0300

    Use pure functional components when appropriate.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
 import './App.css';
 import Dashboard from './components/Dashboard';
 import createStore from './store/store';
+
 const store = createStore;
 
-class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <div className="App">
-        <Dashboard/>          
-        </div>
-      </Provider>
-    );
-  }
-}
+const App = () => (
+  <Provider store={store}>
+    <div className="App">
+      <Dashboard />
+    </div>
+  </Provider>
+);
 
 export default App;
