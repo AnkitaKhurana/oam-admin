dcc20f46ca568179594a10e184765ce7940c445d
Ankita Khurana <ankita0096@gmail.com>
2018-07-29 22:45:20 +0530

    Test Actions

--- test/test_actions.js
+++ test/test_actions.js
@@ -1,5 +1,5 @@
 import test from 'tape';
-import { login, getUsers } from '../src/actions/actions';
+import { login, getUsers, deleteUser, getImages, getUserImages, tokenExpired, tokenValidated, activePageChanged ,currentImagesCalled } from '../src/actions/actions';
 
 test('login', (t) => {
   const username = 'username';
@@ -20,7 +20,6 @@ test('login', (t) => {
   t.end();
 });
 
-
 test('Get Users', (t) => {
   const action = getUsers();
   t.equal(action.type, 'CALL_API');
@@ -30,5 +29,69 @@ test('Get Users', (t) => {
   t.equal(action.payload.types[1], 'FETCH_USERS_SUCCEEDED');
   t.equal(action.payload.types[2], 'FETCH_USERS_FAILED');
   t.equal(action.payload.method, 'GET');
+  t.end();
+});
+
+test('Delete User', (t) => {
+  const action = deleteUser(1234);
+  t.equal(action.type, 'CALL_API');
+  t.equal(action.payload.endpoint, '/users/1234');
+  t.true(action.payload.authenticated);
+  t.equal(action.payload.types[0], 'DELETE_USER');
+  t.equal(action.payload.types[1], 'DELETE_USER_SUCCEEDED');
+  t.equal(action.payload.types[2], 'DELETE_USER_FAILED');
+  t.equal(action.payload.method, 'DELETE');
+  t.end();
+});
+
+test('Get Images', (t) => {
+  const action = getImages();
+  t.equal(action.type, 'CALL_API');
+  t.equal(action.payload.endpoint, '/meta');
+  t.true(action.payload.authenticated);
+  t.equal(action.payload.types[0], 'FETCH_IMAGES');
+  t.equal(action.payload.types[1], 'FETCH_IMAGES_SUCCEEDED');
+  t.equal(action.payload.types[2], 'FETCH_IMAGES_FAILED');
+  t.equal(action.payload.method, 'GET');
+  t.end();
+});
+
+test('Get User Images', (t) => {
+  const action = getUserImages(1234);
+  t.equal(action.type, 'CALL_API');
+  t.equal(action.payload.endpoint, '/users/1234');
+  t.true(action.payload.authenticated);
+  t.equal(action.payload.types[0], 'FETCH_USER_IMAGES');
+  t.equal(action.payload.types[1], 'FETCH_USER_IMAGES_SUCCEEDED');
+  t.equal(action.payload.types[2], 'FETCH_USER_IMAGES_FAILED');
+  t.equal(action.payload.method, 'GET');
+  t.end();
+});
+
+test('Token Expired', (t) => {
+  const action = tokenExpired();
+  t.equal(action.type, 'TOKEN_EXPIRED');
+  t.end();
+});
+
+test('Token Validated', (t) => {
+  const action = tokenValidated();
+  t.equal(action.type, 'TOKEN_VALIDATED');
+  t.end();
+});
+
+test('Active Page Changed', (t) => {
+  const page = 'page';
+  const action = activePageChanged(page);
+  t.equal(action.type, 'ACTIVE_PAGE_CHANGED');
+  t.equal(action.payload, page);
+  t.end();
+});
+
+test('Current Images Called', (t) => {
+  const id = 'id';
+  const action = currentImagesCalled(id);
+  t.equal(action.type, 'CURRENT_IMAGES_CALLED');
+  t.equal(action.payload, id);
   t.end();
 });
